92d8128b97c7c0effb3700fb1d026d09d4de95a2
Mohammad Jabir <mohammadj94@gmail.com>
2024-07-02 09:08:16 -0600

    exercise complete.

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        HttpClient client = new HttpClient();
+
+        while (true)
         {
-            Console.WriteLine("Hello, World");
+            string responseRon = GetResponse("https://ron-swanson-quotes.herokuapp.com/v2/quotes", client);
+            string responseKanye = GetResponse("https://api.kanye.rest", client);
+
+            JArray ron = JArray.Parse(responseRon);
+            JObject kanye = JObject.Parse(responseKanye);
+
+            Console.WriteLine("\n--------------------------------------------------------------------------");
+            Console.WriteLine($"RON: {ron[0]}\n");
+            Console.WriteLine($"KANYE: {kanye["quote"]}");
+            Console.WriteLine("--------------------------------------------------------------------------\n");
+
+            Thread.Sleep(2000);
         }
+    }
+
+    private static string GetResponse(string url, HttpClient client)
+    {
+        return client.GetStringAsync(url).Result;
     }
 }
